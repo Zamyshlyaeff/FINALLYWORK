46e366e961c98dd183eb11f60d3726bad077fc63
Nikolayz <zans55@yandex.ru>
2023-03-15 14:03:24 +0300

    FINALLY VERS

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿string [] array={"ZADA", "NIE", "1234"};
+﻿string [] array={"ZADA","NIE","1234","123","pin"};
 
 int LENGTHARRAY (string[] arr)    //определение длины нового массива
 { int k=0;
@@ -8,12 +8,30 @@ int LENGTHARRAY (string[] arr)    //определение длины новог
   }
   return k;
 }
-void PRINTARRAY(string[]array)
+void PRINTARRAY(string[]array) //печать массива
 {   System.Console.Write("[");
-    for (int i=0;i<array.Length-1;i++)
-{
-    Console.Write($"{array[i]},");
+    for (int i=0;i<array.Length;i++)
+{   if(i<array.Length-1)
+    {
+        Console.Write($"{array[i]},");}
+    else 
+    {
+        Console.Write($"{array[i]}");
+    }
 }
 System.Console.Write("]");
 }
-LENGTHARRAY(array);
+void NEWARRAY(string []array, int k)// новый массив
+{    string[] array1=new string[k];
+     int p=0;
+     for (int i=0;i<array.Length;i++)
+     { if (array[i].Length<=3)
+       {
+       array1[p]=array[i];
+       p++; 
+       }
+     }
+     PRINTARRAY(array1);
+}
+int K=LENGTHARRAY(array);
+NEWARRAY(array,K);
